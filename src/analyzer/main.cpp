c310eba1a4084e012bd7b790b7f89fc87c9a0d7f
Rodrigo <rmagalhaes85@gmail.com>
2011-09-25 00:40:50 -0300

    Removendo diretorio analyzer

--- src/analyzer/main.cpp
+++
@@ -1,1 +0,0 @@
-#include <iostream>#include <cstdio>#include "y.tab.h"int yyparse();int main(int argc, char **argv) {	argc--, argv++;	if (argc > 0) 		yyin = fopen(argv[0], "r");	else		yyin = stdin;			yyparse();	return 0;}
